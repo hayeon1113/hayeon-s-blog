4be457a3709a8dbc28e1257941c8ec8fca4db3dd
hayeon1113 <tina41113@naver.com>
2025-03-13 14:19:32 +0900

    Add files via upload

---
+++ HyProjApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.hayeonproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyProjApplication.class, args);
+	}
+
+}
